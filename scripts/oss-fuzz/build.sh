d28c2aa5a65521f5138e3604ce4d130b79735c1a
cyruscyliu <cyruscyliu@gmail.com>
2021-05-18 15:44:52 +0800

    remove the exit in build.sh

--- scripts/oss-fuzz/build.sh
+++ scripts/oss-fuzz/build.sh
@@ -74,8 +74,6 @@ if ! make "-j$(nproc)" qemu-fuzz-i386 qemu-fuzz-hppa; then
           "\nFor example: CC=clang CXX=clang++ $0"
 fi
 
-exit
-
 for i in $(ldd ./qemu-fuzz-i386 | cut -f3 -d' '); do
     cp "$i" "$DEST_DIR/lib/"
 done
