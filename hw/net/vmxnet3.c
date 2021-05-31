5536d138036adf39b7b1bc2d964fd583ed735a8b
cyruscyliu <cyruscyliu@gmail.com>
2021-05-31 14:14:48 +0800

    Revert "temp fix vmxnet3 g_assert_not_reached bug"
    This reverts commit 5a48d6726340818baba5966a662b7b977211b87f.

--- hw/net/vmxnet3.c
+++ hw/net/vmxnet3.c
@@ -1790,8 +1790,7 @@ vmxnet3_io_bar1_write(void *opaque,
     case VMXNET3_REG_ICR:
         VMW_CBPRN("Write BAR1 [VMXNET3_REG_ICR] = %" PRIx64 ", size %d",
                   val, size);
-        // TODO
-        // g_assert_not_reached();
+        g_assert_not_reached();
         break;
 
     /* Event Cause Register */
