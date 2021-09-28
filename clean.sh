c308f225c77a358a650e3500c1f85c66407f3841
Qiang Liu <cyruscyliu@gmail.com>
2021-09-28 14:26:52 +0200

    mv clean.sh to evaluation

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-cd build-clean
-CC=clang CXX=clang++ ../configure --enable-fuzzing \
-	--target-list="i386-softmmu arm-softmmu aarch64-softmmu"
-ninja qemu-fuzz-i386 qemu-fuzz-arm qemu-fuzz-aarch64
-cd ..
