0b53228263d6c28e8c9b957a5f7c1697ce19cdcc
Qiang Liu <cyruscyliu@gmail.com>
2021-10-03 17:25:02 +0200

    fix xlnx-zynqmp-qspips-00

--- hw/ssi/xilinx_spips.c
+++ hw/ssi/xilinx_spips.c
@@ -921,6 +921,8 @@ static uint64_t xilinx_spips_read(void *opaque, hwaddr addr,
         xilinx_spips_update_ixr(s);
         return ret;
     }
+    if (addr >= 64)
+        return 0;
     DB_PRINT_L(0, "addr=" TARGET_FMT_plx " = %x\n", addr * 4,
                s->regs[addr] & mask);
     return s->regs[addr] & mask;
@@ -1028,6 +1030,8 @@ static void xilinx_spips_write(void *opaque, hwaddr addr,
         try_flush = false;
         break;
     }
+    if (addr >= 64)
+        goto no_reg_update;
     s->regs[addr] = (s->regs[addr] & ~mask) | (value & mask);
 no_reg_update:
     if (try_flush) {
