7a00ea6f9a2950a74b2411e3b4ad4d46f2afd256
Qiang Liu <cyruscyliu@gmail.com>
2022-01-05 11:49:00 +0100

    magage to call groupmutatormiss

--- tests/qtest/fuzz/videzzo_types_stubs.c
+++ tests/qtest/fuzz/videzzo_types_stubs.c
@@ -8,4 +8,16 @@
 
 #include "videzzo.h"
 
-FeedbackHandler group_mutator_handlers[0xff];
+void *group_mutator_handler_test(
+        Input *current_input, uint32_t current_event) {
+    // basically, we have to duplicate the current event
+    // in order to reissue it when GroupMutatorMiss returns
+    Event *orig = get_event(current_input, current_event);
+    Event *copy= (Event *)calloc(sizeof(Event), 1);
+    event_ops[copy->type].deep_copy(orig, copy);
+    insert_event(current_input, copy, current_event + 1);
+}
+
+FeedbackHandler group_mutator_handlers[0xff] = {
+    [1] = group_mutator_handler_test,
+};
